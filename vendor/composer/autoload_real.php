f69f24ebd692923745380a4b4e59dbf9737431cb
Paddy de Búrca <paddy@deburca.org>
2020-04-03 20:29:21 +0200

    Update Drupal core and dependencies

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit583f91f13e94cc759068bcae83fc6f3c
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
